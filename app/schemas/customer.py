090972606c9dc83b6054c3beb3517847bbf87c32
MakyJame <ngoctai1995@gmail.com>
2026-07-30 20:49:41 +0700

    feat: add customer request and response schemas

---
+++ app/schemas/customer.py
@@ -0,0 +1,33 @@
+from pydantic import BaseModel, ConfigDict, Field
+
+class CustomerBase(BaseModel):
+    name: str = Field(
+        min_length=2,
+        max_length=100,
+    )
+
+    phone: str = Field(
+        min_length=8,
+        max_length=20,
+    )
+
+class CustomerCreate(CustomerBase):
+    pass
+
+class CustomerUpdate(BaseModel):
+    name: str | None = Field(
+        default=None,
+        min_length=2,
+        max_length=100,
+    )
+
+    phone: str | None = Field(
+        default=None,
+        min_length=8,
+        max_length=20,
+    )
+
+class  CustomerResponse(CustomerBase):
+    id: int
+
+    model_config = ConfigDict(from_attribute=True)
